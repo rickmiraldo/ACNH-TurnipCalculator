6b560477998bb271deaaacdb0e729b302cb9c8d6
rickmiraldo <rickmiraldo@gmail.com>
2020-04-16 20:39:17 +0100

    Add input data and week prices interfaces

--- ACNH-TurnipCalculator/MainWindow.xaml.cs
+++ ACNH-TurnipCalculator/MainWindow.xaml.cs
@@ -23,6 +23,40 @@ namespace ACNH_TurnipCalculator
         public MainWindow()
         {
             InitializeComponent();
+
+            OnlyEnableSundaysOnCalendar();
+        }
+
+        private void OnlyEnableSundaysOnCalendar()
+        {
+            var minDate = dpBuyingDate.DisplayDateStart ?? DateTime.MinValue;
+            var maxDate = dpBuyingDate.DisplayDateEnd ?? DateTime.MaxValue;
+
+            var firstSunday = FindNextSunday(minDate);
+
+            if (minDate != firstSunday)
+            {
+                var range = new CalendarDateRange(minDate, firstSunday.Subtract(new TimeSpan(1, 0, 0, 0, 0)));
+                dpBuyingDate.BlackoutDates.Add(range);
+            }
+
+            for (var day = firstSunday.AddDays(1); day <= maxDate; day = day.AddDays(7))
+            {
+                var range = new CalendarDateRange(day, day.AddDays(5));
+                dpBuyingDate.BlackoutDates.Add(range);
+            }
+        }
+
+        private DateTime FindNextSunday(DateTime date)
+        {
+            for (var day = date; day <= date.AddDays(7); day = day.AddDays(1))
+            {
+                if (day.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    return day;
+                }
+            }
+            return date;
         }
     }
 }
