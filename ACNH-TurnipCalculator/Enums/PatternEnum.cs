d3b197ed2d5480826445bc7b73d7e15c753f56e7
rickmiraldo <rickmiraldo@gmail.com>
2020-04-18 16:32:02 +0100

    Improve main windows layout

---
+++ ACNH-TurnipCalculator/Enums/PatternEnum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace ACNH_TurnipCalculator.Enums
+{
+    public enum PatternEnum
+    {
+        [Description("Random")]
+        Random,
+
+        [Description("Large Spike")]
+        LargeSpike,
+
+        [Description("Decreasing")]
+        Decreasing,
+
+        [Description("Small Spike")]
+        SmallSpike,
+
+        [Description("I don't remember")]
+        Unkown
+    }
+}
